c709cbb9a7d51acde6b545e3f1fdc07e90c41ba7
riyazur razak <riyazurrazaknoushath786@gmail.com>
2021-06-16 10:56:58 +0530

    static

--- social_app/settings.py
+++ social_app/settings.py
@@ -153,9 +153,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
